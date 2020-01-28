05a56e8685211f7090f3aabf3f8f3a2c8e2e6214
Ryan Freeman <N00173767@student.iadt.ie>
2020-01-28 14:09:18 +0000

    Added code to display trending gifs from Giphy

--- src/components/GiphyViewer.vue
+++ src/components/GiphyViewer.vue
@@ -1,23 +1,39 @@
 <template>
-    <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, inventore.
-    </div>
+    <b-card-group columns>
+        <b-card
+                v-for="gif in gifs"
+                :key="gif.id"
+                :img-src="gif.images.fixed_width.url"
+                :img-alt="gif.title">
+            <b-card-text>
+                <a :href="gif.url" target="_blank">{{gif.title}}</a>
+            </b-card-text>
+        </b-card>
+    </b-card-group>
 </template>
 
 <script>
+    import axios from "axios";
+
+    const GIPHY_URL = "https://api.giphy.com/v1/gifs";
+    const API_KEY = "R6P40k3DHUF3FoJzz3GMf2WgdKD2eaI7";
+
     export default {
         name: "GiphyViewer",
-        components: {
-
-        },
+        components: {},
         data() {
             return {
-
+                gifs: []
             }
         },
-        methods: {
-
-        }
+        mounted() {
+            axios
+                .get(`${GIPHY_URL}/trending?api_key=${API_KEY}`)
+                .then(res => {
+                    this.gifs = res.data.data
+                })
+        },
+        methods: {}
     }
 </script>
 
